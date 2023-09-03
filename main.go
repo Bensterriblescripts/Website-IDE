4b5acaa784c96911de54e594addb500c9203c87b
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-09-03 20:45:57 +1200

    ambitious new project

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// HTTP Server
+	fileServer := http.FileServer(http.Dir("./html"))
+	http.Handle("/", fileServer)
+	fmt.Printf("Starting the HTTP server on port 8000\n")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
