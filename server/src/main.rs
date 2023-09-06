2fe59dadae4f77eabf73af20fd2959dd56bdd41c
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-09-06 19:30:10 +1200

    i am become regex

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,80 +1,99 @@
-// use axum::{
-//     routing::get,
-//     Router,
-// };
+use axum::{
+    extract::Json,
+    routing::post,
+    Router,
+};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GetText {
+    line: String
+}
+
 use regex::Regex;
 use std::time::Instant;
+
+async fn get_text(Json(GetText { line }): Json<GetText>) {
+    send_to_purgatory(&line);
+}
 
 #[tokio::main]
 async fn main() {
 
-    let text = "
-    $test = \"Hello\";
-    if ($text = \"Hello\" && $test != \"Hello\") {
-        echo 'Hi!!';
-    }
-    $newline = 0;
-    ";
+    let app = Router::new().route("/testing", post(get_text));
+
+    axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    
+}
+
+fn send_to_purgatory(text: &str) {
 
     println!("Issues:");
-
     let total_time = Instant::now();
     
     let start_time = Instant::now();
-    variable_check(text);
+    let var = variable_check(text);
     let end_time = Instant::now();
     let elapsed_time = end_time.duration_since(start_time);
     let variable_timer = elapsed_time.as_secs_f64();
 
     let start_time = Instant::now();
-    delimiter_check(text);
+    let delimiter = delimiter_check(text);
     let end_time = Instant::now();
     let elapsed_time = end_time.duration_since(start_time);
     let delimiter_timer = elapsed_time.as_secs_f64();
 
     let start_time = Instant::now();
-    semicolon_check(text);
+    let semicolon = semicolon_check(text);
     let end_time = Instant::now();
     let elapsed_time = end_time.duration_since(start_time);
     let semicolon_timer = elapsed_time.as_secs_f64();
 
     let start_time = Instant::now();
-    condition_check(text);
+    let condition = condition_check(text);
     let end_time = Instant::now();
     let elapsed_time = end_time.duration_since(start_time);
     let condition_timer = elapsed_time.as_secs_f64();
 
     let start_time = Instant::now();
-    string_check(text);
+    let string = string_highlight(text);
     let end_time = Instant::now();
     let elapsed_time = end_time.duration_since(start_time);
     let string_timer = elapsed_time.as_secs_f64();
 
-    println!("\nElapsed times (Milliseconds):\nVariables: {}\nDelimiter: {}\nSemicolon: {}\nConditions: {}\nStrings: {}", variable_timer, delimiter_timer, semicolon_timer, condition_timer, string_timer);
+    let start_time = Instant::now();
+    let function = function_check(text);
+    let end_time = Instant::now();
+    let elapsed_time = end_time.duration_since(start_time);
+    let function_timer = elapsed_time.as_secs_f64();
+
+    println!("\nElapsed times (Milliseconds):\nVariables: {}\nDelimiter: {}\nSemicolon: {}\nConditions: {}\nStrings: {}\nFunctions: {}", variable_timer, delimiter_timer, semicolon_timer, condition_timer, string_timer, function_timer);
 
     let total_end_time = Instant::now();
     let elapsed_time = total_end_time.duration_since(total_time);
     let the_end_timer = elapsed_time.as_secs_f64();
     println!("Total time taken: {}", the_end_timer);
 
-    // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
-    // axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
-    //     .serve(app.into_make_service())
-    //     .await
-    //     .unwrap();
-    
 }
+
+/* ********************/
+/* Checks start here! */
+/* ********************/
+
 fn variable_check(text: &str) {
     // If it needs to be declared
     let var_reg = Regex::new(r"\$[a-zA-Z_][a-zA-Z0-9_]*").unwrap();
     for variable in var_reg.find_iter(text) {
+        println!("Found a variable: {}", variable.as_str()); // Highlighting
         let call_reg = format!(r"{}\s?=[^=].*;", regex::escape(variable.as_str()));
         let call = Regex::new(&call_reg).unwrap();
         if let Some(_capture) = call.find(text) {
             continue;
         }
-        println!("{} needs to be declared", variable.as_str())
+        println!("{} is not set as a variable, declare it somewhere", variable.as_str())
     }
 }
 
@@ -93,8 +112,7 @@ fn delimiter_check(text: &str) {
 }
 
 fn condition_check(text: &str) {
-
-    let cond_reg = Regex::new(r"if.*\(.*=.*\)").unwrap(); // if statement
+    let cond_reg = Regex::new(r"(if|else if|elseif).*\(.*=.*\)").unwrap(); // if statement
     let eq_reg = Regex::new(r"[^=!]=[^=]").unwrap(); // =
 
     for if_state in cond_reg.find_iter(text) {
@@ -105,22 +123,29 @@ fn condition_check(text: &str) {
 }
 
 fn semicolon_check(text: &str) {
-    // let semistring_reg = Regex::new(r"=([^;]*);").unwrap();
+    // let semivar_reg = Regex::new(r"(?s)\$.*=.*;").unwrap();
 
-    // for semicolon in semistring_reg.find_iter(text) {
+    // for semicolon in semivar_reg.find_iter(text) {
     //     println!("Needs a semi-colon: {}", semicolon.as_str());
     // }
 }
 
-fn string_check(text: &str) {
-    let ministring_reg = Regex::new(r"(?s)'.*?'").unwrap();
+fn string_highlight(text: &str) {
+    // let ministring_reg = Regex::new(r"(?s)'.*?'").unwrap();
+    // let biggistring_reg = Regex::new(r#"(?s)".*?""#).unwrap();
 
-    let biggistring_reg = Regex::new(r#"(?s)".*?""#).unwrap();
+    // for stringsmol in ministring_reg.find_iter(text) {
+    //     println!("Found a string: {}", stringsmol.as_str());
+    // }
+    // for stringbig in biggistring_reg.find_iter(text) {
+    //     println!("Found a string: {}", stringbig.as_str());
+    // }
+}
 
-    for stringsmol in ministring_reg.find_iter(text) {
-        println!("Found a string: {}", stringsmol.as_str());
-    }
-    for stringbig in biggistring_reg.find_iter(text) {
-        println!("Found a string: {}", stringbig.as_str());
+fn function_check(text: &str) {
+    let function_reg = Regex::new(r"(?s)[^\$][a-zA-Z_][a-zA-Z0-9_]*\(.*?\)").unwrap();
+
+    for function in function_reg.find_iter(text) {
+        println!("Found function: {}", function.as_str()); // Highlighting
     }
 }
