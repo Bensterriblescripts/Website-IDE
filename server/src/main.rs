65a5b0df0b4d2b18b31aa30490066c9a0b809c4e
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-09-06 17:16:37 +1200

    regex is pain

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,11 +8,12 @@ use std::time::Instant;
 #[tokio::main]
 async fn main() {
 
-    let text = "
+    let text = ";
     $test = \"Hello\";
-    if ($text == \"Hello\") {
+    if ($text == \"Hello\" && $text = \"Hello\") {
         echo 'Hi!!';
     }
+    $newline = 0;
     ";
 
     println!("Issues:");
@@ -35,8 +36,14 @@ async fn main() {
     let elapsed_time = end_time.duration_since(start_time);
     let semicolon_timer = elapsed_time.as_secs_f64();
 
+    let start_time = Instant::now();
+    condition_check(text);
+    let end_time = Instant::now();
+    let elapsed_time = end_time.duration_since(start_time);
+    let condition_timer = elapsed_time.as_secs_f64();
 
-    println!("\nElapsed times (Milliseconds):\nVariables: {}\nDelimiter: {}\nSemicolon: {}", variable_timer, delimiter_timer, semicolon_timer);
+
+    println!("\nElapsed times (Milliseconds):\nVariables: {}\nDelimiter: {}\nSemicolon: {}\nConditions: {}", variable_timer, delimiter_timer, semicolon_timer, condition_timer);
 
     // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
@@ -50,7 +57,7 @@ fn variable_check(text: &str) {
     // If it needs to be declared
     let var_reg = Regex::new(r"\$[a-zA-Z_][a-zA-Z0-9_]*").unwrap();
     for variable in var_reg.find_iter(text) {
-        let call_reg = format!(r"{}\s?=[^=].*", regex::escape(variable.as_str()));
+        let call_reg = format!(r"{}\s?=[^=].*;", regex::escape(variable.as_str()));
         let call = Regex::new(&call_reg).unwrap();
         if let Some(_capture) = call.find(text) {
             continue;
@@ -61,7 +68,7 @@ fn variable_check(text: &str) {
 
 fn delimiter_check(text: &str) {
     // Opened and not closed
-    // let delo_reg = Regex::new(r"\{((?s).*?)\}").unwrap();
+    // let delo_reg = Regex::new(r"\{((.|\n)*)}$").unwrap();
     // for delimiter_open in delo_reg.find_iter(text) {
     //     println!("Delimiter needs to be closed: {}", delimiter_open.as_str());
     // }
@@ -73,9 +80,28 @@ fn delimiter_check(text: &str) {
 
 }
 
-fn semicolon_check(text: &str) {
-    let semi_reg = Regex::new(r"=?(?s)[^=;]").unwrap();
-    for semicolon in semi_reg.find_iter(text) {
-        println!("Needs a semi-colon: {}", semicolon.as_str());
+fn condition_check(text: &str) {
+
+    // ==, !=
+    let cond_reg = Regex::new(r"if.*\(.*=\)").unwrap();
+    let cond_eq_reg = Regex::new(r".*[^=]=[^=].*").unwrap();
+    let cond_neq_reg = Regex::new(r".*[^!]=.*").unwrap();
+    for condition in cond_reg.find_iter(text) {
+        if let Some(_capture) = cond_eq_reg.find(text) {
+            continue;
+        }
+        if let Some(_capture) = cond_neq_reg.find(text) {
+            continue;
+        }
+        
+        println!("'{}' is missing another operand (needs == or !=)", condition.as_str())
     }
+}
+
+fn semicolon_check(text: &str) {
+    // let semistring_reg = Regex::new(r"(?s)[^=]=\s[^=](.*[^;])").unwrap();
+
+    // for semicolon in semistring_reg.find_iter(text) {
+    //     println!("Needs a semi-colon: {}", semicolon.as_str());
+    // }
 }
